699cb247063c331bdb5d9a9e837d5be789969f1f
Shuffled72 <shuffled720@gmail.com>
2023-07-14 21:06:27 +0530

    db set

--- database/db.js
+++ database/db.js
@@ -7,8 +7,11 @@ const Connection = async (username, password) => {
     mongoose.set('strictQuery', true);
     // const URL = `mongodb://${username}:${password}@blogweb-shard-00-00.ch1hk.mongodb.net:27017,blogweb-shard-00-01.ch1hk.mongodb.net:27017,blogweb-shard-00-02.ch1hk.mongodb.net:27017/BLOG?ssl=true&replicaSet=atlas-lhtsci-shard-0&authSource=admin&retryWrites=true&w=majority`;
     // const URL='mongodb://localhost:27017/AAA'
-    const URL='mongodb://127.0.0.1:27017'
+    // const URL='mongodb://127.0.0.1:27017'
+    const URL=`mongodb+srv://${username}:${password}@cluster0.gayb9xx.mongodb.net/?retryWrites=true&w=majority`
+    // const URL=`mongodb+srv://root:<password>@cluster0.gayb9xx.mongodb.net/?retryWrites=true&w=majority`
     // const URL=`mongodb+srv://${username}:${password}@cluster0.gayb9xx.mongodb.net/?retryWrites=true&w=majority`
+
     try {
         await mongoose.connect(URL, { useNewUrlParser: true })
         console.log('Database connected successfully');
