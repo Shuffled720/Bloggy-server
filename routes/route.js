47f425ed757e1e2905cf6e04771a1611037ff50a
Shuffled72 <shuffled720@gmail.com>
2023-07-14 20:03:11 +0530

    version changed

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import { createPost, updatePost, deletePost, getPost, getAllPosts } from '../controller/post-controller.js';
-import { uploadImage, getImage } from '../controller/image-controller.js';
+// import { uploadImage, getImage } from '../controller/image-controller.js';
 import { newComment, getComments, deleteComment } from '../controller/comment-controller.js';
 import { loginUser, singupUser, logoutUser } from '../controller/user-controller.js';
 import { authenticateToken, createNewToken } from '../controller/jwt-controller.js';
@@ -23,8 +23,8 @@ router.delete('/delete/:id', authenticateToken, deletePost);
 router.get('/post/:id', authenticateToken, getPost);
 router.get('/posts', authenticateToken, getAllPosts);
 
-router.post('/file/upload', upload.single('file'), uploadImage);
-router.get('/file/:filename', getImage);
+// router.post('/file/upload', upload.single('file'), uploadImage);
+// router.get('/file/:filename', getImage);
 
 router.post('/comment/new', authenticateToken, newComment);
 router.get('/comments/:id', authenticateToken, getComments);
